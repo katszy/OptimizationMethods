7d03dde93389632210191424ff9f785d70ac684a
Szondy Kata <szondy.kata@gmail.com>
2023-05-28 17:15:11 +0200

    major fix

--- scheduling.py
+++ scheduling.py
@@ -49,10 +49,10 @@ def make_two_rand_int(process_times):
 def print_result(result_schedule,result_makespan):
   print("Schedule:")
   for machine, jobs in enumerate(result_schedule):
-      print(f"Machine {machine + 1}: {jobs}")
+      print(f"Machine {machine+1}: {jobs}")
   print("Makespan:", result_makespan)
 
-def swap(process_times, calculated_schedule,calculated_makespan, recalculations_num):
+def permutation_swap(process_times, calculated_schedule,calculated_makespan, recalculations_num):
   better_solution = 0
   original_makespan = calculated_makespan
   for i in range(recalculations_num):
@@ -68,45 +68,19 @@ def swap(process_times, calculated_schedule,calculated_makespan, recalculations_
     
     if(test_makespan < original_makespan):
       better_solution += 1
-
+    
 #swap list if it is better solution
     if(test_makespan < calculated_makespan):
       process_times = saved_process_times
       calculated_makespan = test_makespan
       calculated_schedule = test_schedule
+    
   
-  print("Swapping 2 jobs, improvement ratio:")
+  print("list permutation, swapping, improvement ratio:")
   print(better_solution/recalculations_num*100)
   return calculated_schedule,calculated_makespan
 
-
-def move(process_times, calculated_schedule,calculated_makespan, recalculations_num):
-  original_makespan = calculated_makespan
-  better_solution = 0
-  for i in range(recalculations_num):
-    saved_process_times = copy.deepcopy(process_times)
-    jobs_indexes = len(process_times[0]) - 1
-    random_int = int(random.randint(0,jobs_indexes-1))
-    #swap elements
-    for i in saved_process_times:
-        i[random_int], i[jobs_indexes] = i[jobs_indexes], i[random_int]
-    #calculate jobs with new table
-    test_schedule, test_makespan = list_scheduling(saved_process_times)
-
-    if(test_makespan < original_makespan):
-      better_solution += 1
-
-    #swap list if it is better solution
-    if(test_makespan < calculated_makespan):
-      process_times = saved_process_times
-      calculated_makespan = test_makespan
-      calculated_schedule = test_schedule
-
-  print("Random to the end, improvement ratio:")
-  print(better_solution/recalculations_num*100)
-  return calculated_schedule,calculated_makespan
-
-def find_tail_and_swap(process_times, calculated_schedule,calculated_makespan, recalculations_num):
+def permutation_tail_to_front(process_times, calculated_schedule,calculated_makespan, recalculations_num):
   better_solution = 0
   original_makespan = calculated_makespan
   for i in range(recalculations_num):
@@ -130,42 +104,88 @@ def find_tail_and_swap(process_times, calculated_schedule,calculated_makespan, r
       calculated_makespan = test_makespan
       calculated_schedule = test_schedule
 
-  print("Tail to the end, improvement ratio:")
+  print("list permutation, tail to the end, improvement ratio:")
   print(better_solution/recalculations_num*100)
   return calculated_schedule,calculated_makespan
 
-def swap_and_move(process_times, calculated_schedule,calculated_makespan, recalculations_num):
-  better_solution = 0
-  original_makespan = calculated_makespan
-  for i in range(recalculations_num):
-    if(random.random()) < 0.5:
-         saved_process_times = copy.deepcopy(process_times)
-         first_element, second_element = make_two_rand_int(saved_process_times)
-         for i in saved_process_times:
-          i[first_element], i[second_element] = i[second_element], i[first_element]
-          test_schedule, test_makespan = list_scheduling(saved_process_times)
-    else:
-        saved_process_times = copy.deepcopy(process_times)
-        jobs_indexes = len(process_times[0]) - 1
-        random_int = int(random.randint(0,jobs_indexes-1))
-        for i in saved_process_times:
-          i[random_int], i[jobs_indexes] = i[jobs_indexes], i[random_int]
-          test_schedule, test_makespan = list_scheduling(saved_process_times)
-
-    if(test_makespan < original_makespan):
-      better_solution += 1
-      
-    if(makespan < calculated_makespan):
-      process_times = saved_process_times
-      calculated_makespan = test_makespan
-      calculated_schedule = test_schedule
-
-  print("swap and move, improvement ratio:")
-  print(better_solution/recalculations_num*100)
-  return calculated_schedule,calculated_makespan
+def calculate_makespan(sch):
+  makespan = 0
+  idx = 0
+  for m_index, machine in enumerate(sch):
+    machine_makespan = 0
+    for job in machine:
+      job_index = job - 1
+      job_processing_time = processing_times[m_index][job_index][1]
+      machine_makespan += job_processing_time
+      if machine_makespan > makespan:
+        makespan = machine_makespan
+        idx = m_index
+  return makespan, idx
     
-    
+def move(ls_schedule, ls_makespan):
+  best = ls_makespan
+  for i in range(1):
+    #choose a machine that deteremines makespan
+    sch = copy.deepcopy(ls_schedule)
+    makespan, random_machine = calculate_makespan(sch) #random_machine is not actually random!!!  #it's the one that determines the makespan! 
+    m = sch[random_machine]
+    #choose a random job 
+    random_job_index = random.randint(0, len(m) - 1)
+    job = processing_times[random_machine][m[random_job_index]-1]
+    j = job[0]
+    #put it elsewhere to the end, choose machine 
+    dest_machine = random_machine
+    while (dest_machine==random_machine):
+      dest_machine = random.choice([0, 1, 2])
+    dest_machine = sch[dest_machine]
+    #place job at the end
+    m.pop(random_job_index)
+    dest_machine.append(j)
 
+    #calculate new makespan 
+    makespan, idx = calculate_makespan(sch)
+  if makespan <= best:
+    best = makespan 
+  else:
+    sch = ls_schedule
+    best = ls_makespan
+  return sch,best
+  
+def swap(ls_schedule, ls_makespan):
+  best = ls_makespan
+  for i in range(1):
+    #choose a machine that deteremines makespan
+    sch = copy.deepcopy(ls_schedule)
+    makespan, random_machine = calculate_makespan(sch) #random_machine is not actually random!!!  #it's the one that determines the makespan! 
+    m = sch[random_machine]
+    #choose a random job 
+    random_job_index = random.randint(0, len(m) - 1)
+    job = processing_times[random_machine][m[random_job_index]-1]
+    j = job[0]
+    
+    #choose a random machine
+    dest_machine = random_machine
+    while (dest_machine==random_machine):
+      dest_machine = random.choice([0, 1, 2])
+    md = sch[dest_machine]
+    #choose a random job 
+    random_job_index_2 = random.randint(0, len(md) - 1)
+    job = processing_times[random_machine][md[random_job_index_2]-1]
+    jd = job[0]
+  
+    #swap them
+    m[random_job_index] = jd
+    md[random_job_index_2] = j
+
+    #calculate new makespan 
+    makespan, idx = calculate_makespan(sch)
+  if makespan <= best:
+    best = makespan  
+  else:
+    sch = ls_schedule
+    best = ls_makespan
+  return sch, best
+    
 # Define the processing time table
 processing_times = [
     [[1,1], [2,8], [3,3], [4,4], [5,3], [6,1], [7,6], [8,5], [9,3], [10,1], [11,2], [12,3], [13,1], [14,2], [15,3], [16,4], [17,3], [18,2], [19,2], [20,5], [21,2], [22,1], [23,2], [24,3]],
@@ -176,14 +196,27 @@ processing_times = [
 # Run the list scheduling algorithm
 ls_schedule, ls_makespan = list_scheduling(processing_times)
 print_result(ls_schedule, ls_makespan)
+print("__________________________________")
 
-schedule, makespan = swap(processing_times, ls_schedule, ls_makespan,1000)
-print_result(schedule,makespan)
-schedule, makespan = move(processing_times, ls_schedule, ls_makespan,1000)
-print_result(schedule,makespan)
-schedule, makespan = find_tail_and_swap(processing_times, ls_schedule, ls_makespan,1000)
-print_result(schedule,makespan)
-schedule, makespan = swap_and_move(processing_times, ls_schedule, ls_makespan,1000)
-#print("FINAL RESULT:::::::::::")
-print_result(schedule,makespan)
+#first possibility: move and swap
+sch = ls_schedule
+makespan = ls_makespan 
+for i in range(2000):
+  if random.random() < 0.5:
+    sch, makespan = move(sch, makespan)
+  else: 
+    sch, makespan = swap(sch, makespan)
+print("SWAP AND MOVE:")
+print_result(sch,makespan)
+print("__________________________________")
 
+#second possibility: permutating the list 
+# Run the list scheduling algorithm
+ls_schedule, ls_makespan = list_scheduling(processing_times)
+schedule, makespan =permutation_swap(processing_times, ls_schedule, ls_makespan, 2000)
+print("LIST PERMUTATION, SWAPPING:")
+print_result(schedule,makespan)
+print("__________________________________")
+schedule, makespan = permutation_tail_to_front(processing_times, ls_schedule, ls_makespan, 2000)
+print("LIST PERMUTATION, TAIL TO FRONT:")
+print_result(schedule,makespan)
